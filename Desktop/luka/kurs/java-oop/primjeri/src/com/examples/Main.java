de153212c5c90a5f6d7ddfa10f9cddfe3b08399a
SinisaGutovic <s.gutovic@levi9.com>
2021-09-23 21:34:28 +0200

    helloworld:part 1

---
+++ Desktop/luka/kurs/java-oop/primjeri/src/com/examples/Main.java
@@ -0,0 +1,21 @@
+//ime paketa u kome se nalazi klasa
+//paket predstvalja grupu srodnih klasa
+//imena paketa se pisu malim slovom a ako ih ima vise odvajau se tackom
+package com.examples;
+
+//klasa Main
+//javi sve je objekat
+//za pocetak klasu definisemo po formi public class IME_KLASE {}
+//imena klasa se pisu velikim slovima
+public class Main {
+
+    //metoda main je pocetna tacka svakog programa
+    //postoji samo jedna metoda main kao ulazna tacka u svakom programu
+    //imena metoda se pisu malim slovima
+    public static void main(String[] args) {
+        //naredba kojom se ispisuje tekst tj. string Hello World na konzoli(terminalu)
+        //naredbom se moze nazvati svaka linija koda u formi IZRAZ_NAREDBE;
+        //na kraju svake naredbe stoji ; koja oznacava kraj naredbe
+        System.out.println("Hello World");
+    }
+}
